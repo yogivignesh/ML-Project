ffec1fe277bb875f114868c560660c99932fe8db
yogivignesh03@gmail.com <yogivignesh03@gmail.com>
2026-04-26 10:39:48 +0530

    Initial clean commit

---
+++ src/components/data_transformation.py
@@ -0,0 +1,19 @@
+import sys
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+import os
+
+from src.utils import save_object
+
+@dataclass
+def DataTransformationConfig():
+    preprocessor_obj_file_path = os.path.join("a")
